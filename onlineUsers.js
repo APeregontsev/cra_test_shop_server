b91a7893221638c76f93b0be76bfadcb9a605d52
Alexander Peregontsev <110561969+APeregontsev@users.noreply.github.com>
2023-07-09 06:41:36 +0300

    Update onlineUsers.js

--- onlineUsers.js
+++ onlineUsers.js
@@ -17,7 +17,7 @@ module.exports = function (req, res, connection, next) {
       console.log("INSERTED INTO ONLINE >>>>>>>>>>>>", resultInsert);
 
       // Setting cookie
-      res.cookie("user_id", hash, {  expires: new Date(Date.now() + 43_200_000), sameSite = "None"});
+      res.cookie("user_id", hash, {  expires: new Date(Date.now() + 43_200_000), sameSite: "None"});
       next();
     });
   } else {
